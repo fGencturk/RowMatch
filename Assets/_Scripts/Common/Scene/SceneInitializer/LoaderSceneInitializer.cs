1bf568b859d84340f58539d90bd4c9c96bb89bf3
Furkan Gençtürk <gencturk.furkan@gmail.com>
2022-09-04 16:41:27 +0300

    feat(Rotation): disable auto rotation after game starts

--- Assets/_Scripts/Common/Scene/SceneInitializer/LoaderSceneInitializer.cs
+++ Assets/_Scripts/Common/Scene/SceneInitializer/LoaderSceneInitializer.cs
@@ -27,6 +27,21 @@ namespace Common.Scene.SceneInitializer
             BindInstance(new LevelLoadController());
             BindInstance(new PlayerData());
             UIHelper2D.Initialize();
+            
+            // After game starts portrait/landscape lock orientation
+            // TODO what about splash?
+            if (Screen.width > Screen.height)
+            {
+                Screen.orientation = ScreenOrientation.LandscapeLeft;
+                Screen.autorotateToPortrait = false;
+                Screen.autorotateToPortraitUpsideDown = false;
+            }
+            else
+            {
+                Screen.orientation = ScreenOrientation.Portrait;
+                Screen.autorotateToLandscapeLeft = false;
+                Screen.autorotateToLandscapeRight = false;
+            }
         }
 
         protected override void Initialize()
